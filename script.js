57c163ae961920a86ce111993bf9773d9a97629d
abimanyusatyadji <96362718+abimanyusatyadji@users.noreply.github.com>
2022-03-31 21:14:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ let prevNum = "";
 let calcOpr = "";
 let currNum = "";
 const inputNumber = (number) => {
-    currNum += number;
+    if (currNum === "0") {
+        currNum = number;
+    } else {
+        currNum += number;
+    }
 }
 
 numbers.forEach((number) => {
@@ -22,4 +26,71 @@ numbers.forEach((number) => {
         inputNumber(event.target.value)
         updateScreen(currNum);
     })
+})
+
+const oper = document.querySelectorAll(".operator");
+oper.forEach((opr) => {
+    opr.addEventListener("click", (event) => {
+        inptOpr(event.target.value);
+    })
+})
+
+const inptOpr = (oper) => {
+    prevNum = currNum
+    calcOpr = oper
+    currNum = ""
+}
+
+const equalSign = document.querySelector(".equal-sign");
+equalSign.addEventListener("click", () => {
+    calculate();
+    updateScreen(currNum);
+})
+
+const calculate = () => {
+    let result = "";
+    switch (calcOpr) {
+        case "+":
+            result = parseFloat(prevNum) + parseFloat(currNum)
+            break
+        case "-":
+            result = parseFloat(prevNum) - parseFloat(currNum)
+            break
+        case "*":
+            result = parseFloat(prevNum) * parseFloat(currNum)
+            break
+        case "/":
+            result = parseFloat(prevNum) / parseFloat(currNum)
+            break
+        case "%":
+            result = parseFloat(prevNum) / parseFloat(currNum)
+            break
+        default:
+            break
+    }
+    currNum = result
+    calcOpr = ""
+}
+
+const clearAll = () => {
+    prevNum = ""
+    calcOpr = ""
+    currNum = "0"
+}
+const clearBtn = document.querySelector(".all-clear");
+clearBtn.addEventListener("click", () => {
+    clearAll();
+    updateScreen(currNum);
+})
+
+inputDecimal = (dot) => {
+    if (currNum.includes('.')) {
+        return;
+    }
+    currNum += dot
+}
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", (event) => {
+    inputDecimal(event.target.value);
+    updateScreen(currNum);
 })
